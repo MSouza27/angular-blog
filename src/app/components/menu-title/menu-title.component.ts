f31e37e17631a33e56c1486dcbd7c22574f93428
magnoclear <magnoapt@gmail.com>
2025-02-22 20:16:26 -0300

    components creados com estilos

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
-  imports: [],
   templateUrl: './menu-title.component.html',
-  styleUrl: './menu-title.component.css'
+  styleUrls: ['./menu-title.component.css']
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit {
+  fullText: string = "MAGNO BLOG";
+  displayedText: string = "";
+  index: number = 0;
 
+  ngOnInit() {
+    this.animateText();
+  }
+
+  animateText() {
+    if (this.index < this.fullText.length) {
+      this.displayedText += this.fullText[this.index];
+      this.index++;
+      setTimeout(() => this.animateText(), 150); // Tempo entre as letras
+    }
+  }
 }
+
